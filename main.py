9f909aeda365d7c32015351ccbab2232eeeaa5da
AidanJP <aidanpendlebury@gmail.com>
2021-02-06 00:22:00 +0000

    More pages added. Ability to add purchase to cart.

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-from flask import Flask, render_template, redirect, url_for, flash, abort, jsonify
+from flask import Flask, render_template, redirect, url_for, flash, abort, request
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.orm import relationship
+from sqlalchemy.orm import relationship, load_only
 from flask_login import UserMixin, login_user, LoginManager, current_user, logout_user
 from forms import CreateProductForm, CreateCustomerForm, LogInForm
 from functools import wraps
@@ -12,7 +12,6 @@ from flask_bootstrap import Bootstrap
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.getenv("SECRET_KEY")
 Bootstrap(app)
-
 
 # CONNECT TO DB
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL", "sqlite:///shop.db")
@@ -39,12 +38,12 @@ class Customer(UserMixin, db.Model):
 class Purchase(db.Model):
     __tablename__ = "purchases"
     id = db.Column(db.Integer, primary_key=True)
-    body = db.Column(db.Text, nullable=False)
     paid = db.Column(db.Boolean, nullable=False)
 
     # Relationships
     purchaser_id = db.Column(db.Integer, db.ForeignKey("customers.id"))
     purchaser = relationship("Customer", back_populates="purchases")
+    product_id = db.Column(db.Integer, db.ForeignKey("products.id"))
     product = relationship("Product", back_populates="purchase")
 
 
@@ -56,7 +55,6 @@ class Product(db.Model):
     img_url = db.Column(db.String(250), nullable=False)
 
     # Relationships
-    purchase_id = db.Column(db.Integer, db.ForeignKey("purchases.id"))
     purchase = relationship("Purchase", back_populates="product")
 
 
@@ -82,8 +80,16 @@ def admin_only(f):
 
 @app.route('/')
 def product_page():
-    products = Product.query.all()
-    return render_template("index.html", all_products=products, current_user=current_user)
+    in_cart = db.session.query(Purchase).filter(Purchase.purchaser_id == current_user.get_id(),
+                                                Purchase.paid is not True)
+    user_pur_ids = [pur.id for pur in in_cart]
+
+    purchased_by_others = db.session.query(Purchase).filter(Purchase.purchaser_id != current_user.get_id())
+    other_pur_ids = [pur.id for pur in purchased_by_others]
+
+    # Reduce the list of available products that are not reserved/purchased by others
+    products_reduced = [product for product in Product.query.all() if product.id not in other_pur_ids]
+    return render_template("index.html", all_products=products_reduced, current_user=current_user, in_cart=user_pur_ids)
 
 
 @app.route('/register', methods=["GET", "POST"])
@@ -140,12 +146,27 @@ def logout():
 
 @app.route("/cart")
 def cart():
+    if current_user.is_anonymous:
+      return redirect(url_for('login'))
     return render_template("cart.html")
+
+
+@app.route("/add-to-cart")
+def add_to_cart():
+    product_id = request.args.get("product_id")
+    print(f"The product id is {product_id}")
+    if not current_user.is_anonymous:
+        # Add new purchase
+        purchase = Purchase(paid=False, purchaser_id=current_user.get_id(), product_id=product_id)
+        db.session.add(purchase)
+        db.session.commit()
+        return redirect(url_for("product_page"))
+    return redirect(url_for('login'))
 
 
 @app.route("/new-product", methods=["GET", "POST"])
 @admin_only
-def add_new_post():
+def add_new_product():
     form = CreateProductForm()
     if form.validate_on_submit():
         new_product = Product(
